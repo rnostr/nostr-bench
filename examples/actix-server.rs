0724b9a2707eaf9eba15cf94594937d8345deeed
Arron <arronzhang@me.com>
2023-02-27 21:37:28 +0800

    Add actix server

---
+++ examples/actix-server.rs
@@ -0,0 +1,82 @@
+use actix::prelude::*;
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
+use log::{debug, error};
+use std::{env, net::SocketAddr};
+
+/// websocket connection is long running connection, it easier
+/// to handle with an actor
+
+pub struct MyWebSocket {
+    addr: SocketAddr,
+}
+
+impl Actor for MyWebSocket {
+    type Context = ws::WebsocketContext<Self>;
+
+    /// Method is called on actor start. We start the heartbeat process here.
+    fn started(&mut self, _ctx: &mut Self::Context) {
+        debug!("start {}", self.addr);
+    }
+
+    fn stopped(&mut self, _ctx: &mut Self::Context) {
+        debug!("stop {}", self.addr);
+    }
+}
+
+/// Handler for `ws::Message`
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        debug!("WEBSOCKET MESSAGE: {msg:?}");
+        let msg = match msg {
+            Err(err) => {
+                error!("Receive error: {:?}", err);
+                ctx.stop();
+                return;
+            }
+            Ok(msg) => msg,
+        };
+        match msg {
+            ws::Message::Text(_text) => {}
+            ws::Message::Ping(msg) => ctx.pong(&msg),
+            ws::Message::Close(reason) => {
+                ctx.close(reason);
+                ctx.stop();
+            }
+            _ => {}
+        }
+    }
+}
+
+/// WebSocket handshake and start `MyWebSocket` actor.
+async fn echo_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    ws::start(
+        MyWebSocket {
+            addr: req.peer_addr().unwrap(),
+        },
+        &req,
+        stream,
+    )
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let _ = env_logger::try_init();
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    println!("Listening on: {}", addr);
+
+    HttpServer::new(|| {
+        App::new()
+            // websocket route
+            .service(web::resource("/").route(web::get().to(echo_ws)))
+        // enable logger
+        // .wrap(middleware::Logger::default())
+    })
+    // .workers(2)
+    .bind(addr)?
+    .run()
+    .await
+}
