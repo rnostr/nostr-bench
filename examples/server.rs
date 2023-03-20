f8d54daf97cc2878f1442a234b9921d2c089003c
Arron <arronzhang@me.com>
2023-03-20 11:07:34 +0800

    Add receive batch message example

--- examples/server.rs
+++ examples/server.rs
@@ -7,8 +7,9 @@
 
 use std::env;
 
+use flume::{unbounded, Sender};
 use futures_util::{SinkExt, StreamExt};
-use log::debug;
+use log::{debug, info};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::tungstenite::Message;
 
@@ -24,11 +25,27 @@ async fn main() -> Result<(), std::io::Error> {
     let listener = try_socket.expect("Failed to bind");
     println!("Listening on: {}", addr);
 
+    let (tx, rx) = unbounded();
+    tokio::spawn(async move {
+        while let Ok(i) = rx.recv_async().await {
+            let mut batch = vec![i];
+            let len = rx.len();
+            for _l in 0..len {
+                if let Ok(i) = rx.recv() {
+                    batch.push(i);
+                }
+            }
+            tokio::time::sleep(tokio::time::Duration::from_millis(50)).await;
+            info!("batch received: {:?}", batch.len());
+        }
+    });
+
     loop {
+        let tx_c = tx.clone();
         match listener.accept().await {
             Ok((stream, _)) => {
-                tokio::spawn(async {
-                    let res = accept_connection(stream).await;
+                tokio::spawn(async move {
+                    let res = accept_connection(stream, tx_c).await;
                     if let Err(err) = res {
                         debug!("WebSocket error: {}", err);
                     }
@@ -43,7 +60,10 @@ async fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-async fn accept_connection(stream: TcpStream) -> Result<(), tokio_tungstenite::tungstenite::Error> {
+async fn accept_connection(
+    stream: TcpStream,
+    tx: Sender<String>,
+) -> Result<(), tokio_tungstenite::tungstenite::Error> {
     let addr = stream.peer_addr()?;
     debug!("Peer address: {}", addr);
 
@@ -60,9 +80,11 @@ async fn accept_connection(stream: TcpStream) -> Result<(), tokio_tungstenite::t
             Some(msg) => {
                 let msg = msg?;
                 if msg.is_text() {
-                    if msg.to_string().contains("EVENT") {
+                    let msg = msg.to_string();
+                    let _ = tx.send(msg.clone());
+                    if msg.contains("EVENT") {
                         write.send(Message::Text(r#"["OK", "b1a649ebe8b435ec71d3784793f3bbf4b93e64e17568a741aecd4c7ddeafce30", true, ""]"#.to_string())).await?;
-                    } else if msg.to_string().contains("REQ") {
+                    } else if msg.contains("REQ") {
                         write
                             .send(Message::Text(r#"["EOSE", "sub"]"#.to_string()))
                             .await?;
